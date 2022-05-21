7c2bdf4fd1aabe98bb5ff15b364e8fb71b46def6
astrapi69 <asterios.raptis@gmx.net>
2022-05-21 16:25:19 +0200

    added new integration tests

--- src/test/java/io/github/astrapi69/crypt/info/jpa/repository/TreeNodeInfosRepositoryTest.java
+++ src/test/java/io/github/astrapi69/crypt/info/jpa/repository/TreeNodeInfosRepositoryTest.java
@@ -2,7 +2,9 @@ package io.github.astrapi69.crypt.info.jpa.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Test;
@@ -17,7 +19,6 @@ class TreeNodeInfosRepositoryTest extends AbstractIntegrationTest
 	@Autowired
 	TreeNodeInfosRepository repository;
 
-
 	@Test
 	public void testSave()
 	{
@@ -28,9 +29,12 @@ class TreeNodeInfosRepositoryTest extends AbstractIntegrationTest
 		TreeNodeInfos root = TreeNodeInfos.builder().parent(null).depth(0).node(true)
 			.value("I'm root").build();
 
-
 		TreeNodeInfos aChild = TreeNodeInfos.builder().parent(root).depth(1).node(true)
 			.value("I'm a child").build();
+		TreeNodeInfos anotherChild = TreeNodeInfos.builder().parent(root).depth(1).node(true)
+			.value("I'm an another child").build();
+		TreeNodeInfos aChildChild = TreeNodeInfos.builder().parent(aChild).depth(2).node(true)
+			.value("I'm a grand child").build();
 
 		TreeNodeInfos saved = repository.save(root);
 		assertNotNull(saved);
@@ -39,9 +43,24 @@ class TreeNodeInfosRepositoryTest extends AbstractIntegrationTest
 		repository.findById(saved.getId()).ifPresent(ent -> atomicReference.set(ent));
 		assertEquals(saved, atomicReference.get());
 
+		saved = repository.save(anotherChild);
+
+		repository.findById(saved.getId()).ifPresent(ent -> atomicReference.set(ent));
+		assertEquals(saved, atomicReference.get());
+
 		saved = repository.save(aChild);
 
 		repository.findById(saved.getId()).ifPresent(ent -> atomicReference.set(ent));
 		assertEquals(saved, atomicReference.get());
+
+		saved = repository.save(aChildChild);
+
+		repository.findById(saved.getId()).ifPresent(ent -> atomicReference.set(ent));
+		assertEquals(saved, atomicReference.get());
+
+		List<TreeNodeInfos> children = repository.getChildren(root.getId());
+
+		assertTrue(children.contains(aChild));
+		assertTrue(children.contains(anotherChild));
 	}
 }
